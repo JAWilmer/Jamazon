6c3dc4cfa12c8539b70dc39d2513435394ca1bf5
Wilmer <julieannewilmer@gmail.com>
2018-10-22 15:22:12 -0500

    added 'add inventory' to manager app

--- jamazonManager.js
+++ jamazonManager.js
@@ -16,81 +16,157 @@ connection.connect(function (err) {
 });
 
 let managerDuties = function () {
-    // query the database for all products available for purchase
-    connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
-        // once you have the products, prompt the user for which they'd like to purchase
-        inquirer.prompt([
-            {
-                name: "manageInventory",
-                type: "list",
-                message: "\n\nHowdy, Manager!  How would you like to manage your inventory?",
-                choices: [
-                    'View Products for Sale',
-                    'View Low Inventory',
-                    'Add to Inventory',
-                    'Add New Product',
-                ]
-            }
-        ]).then(function (manageInventoryAnswer) {
+    inquirer.prompt([{
+        name: "manageInventory",
+        type: "list",
+        message: "\n\nHowdy, Manager!  How would you like to manage your inventory?",
+        choices: [
+            'View Products for Sale',
+            'View Low Inventory',
+            'Add to Inventory',
+            'Add New Product',
+            'Exit'
+        ]
+    }]).then(function (manageInventoryAnswer) {
 
-            switch (manageInventoryAnswer.manageInventory) {
-                case "View Products for Sale":
-                    connection.query("SELECT * FROM products", function (err, results) {
-                        if (err) throw err;
-                        console.table(results);
-                        managerDuties();
-                    });
-                    break;
-                case "View Low Inventory":
-                    connection.query("SELECT * FROM products WHERE stock_quantity < 25", function (err, results) {
-                        if (err) throw err;
-                        console.table(results);
-                        managerDuties();
-                    });
-                    break;
-                case "Add to Inventory":
-                    inquirer.prompt([
-                        {
-                            name: "product",
-                            type: "list",
-                            message: "Which product would you like to restock:",
-                            choices: function () {
-                                let productArray = [];
-                                for (var i = 0; i < results.length; i++) {
-                                    productArray.push(results[i].product_name);
-                                }
-                                return productArray;
-                            }
-                        },
-                        {
-                            name: "quantity",
-                            type: "input",
-                            message: "How many items would you like to add?"
-                        }
-                    ]).then(function (restockAnswer) {
-                        console.log("restock answer: ", restockAnswer)
-                        console.log("results[0].stock_quantity", results[0].stock_quantity)
-                        console.log('restockAnswer.quantity', restockAnswer.quantity)
-                        // do i need to parseInt?
-                        let updatedQuantity = parseInt(results[0].stock_quantity) + parseInt(restockAnswer.quantity);
-                        let productName = restockAnswer.product
-                        connection.query('UPDATE products SET ? WHERE ?',
-                            [{
-                                stock_quantity: updatedQuantity
-                            },
-                            {
-                                product_name: productName
-                            }],
-                            function (err, results) {
-                                if (err) throw err;
-                                console.log(results)
-                                console.log(`${productName} stock has been updated to: ${updatedQuantity}.`);
-                                managerDuties();
-                            });
-                    })
-            }
-        })
+        switch (manageInventoryAnswer.manageInventory) {
+            case "View Products for Sale":
+                productsForSale();
+                break;
+            case "View Items with Low Inventory":
+                lowInventory();
+                break;
+            case "Add to Inventory":
+                addInventory();
+                break;
+            case "Add New Product":
+                addProduct();
+                break;
+            case 'EXIT':
+                process.exit();
+        }
     })
 }
 managerDuties();
+
+function productsForSale() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        console.table(results);
+        managerDuties();
+    });
+}
+
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 25", function (err, results) {
+        
+        if (err) {
+             throw err 
+        } else if (!err) {
+            console.table(results);
+        } else {
+            console.lot(`You have 25 or more of each item in your inventory. Your store is fully stocked.`)
+        }
+        managerDuties();
+    });
+}
+
+function addInventory() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "product",
+                type: "list",
+                message: "Which product would you like to restock:",
+                choices: function () {
+                    let productArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        productArray.push(results[i].product_name);
+                    }
+                    return productArray;
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many items would you like to add?"
+            }
+        ]).then(function (restockAnswer) {
+            // add in nan info
+            console.log("restock answer: ", restockAnswer)
+            console.log("results[0].stock_quantity", results[0].stock_quantity)
+            console.log('restockAnswer.quantity', restockAnswer.quantity)
+            let updatedQuantity = parseInt(results[0].stock_quantity) + parseInt(restockAnswer.quantity);
+            let productName = restockAnswer.product
+            connection.query('UPDATE products SET ? WHERE ?',
+                [{
+                    stock_quantity: updatedQuantity
+                },
+                {
+                    product_name: productName
+                }],
+                function (err, results) {
+                    if (err) throw err;
+                    console.log(results)
+                    console.log(`${productName} stock has been updated to: ${updatedQuantity}.`);
+                    managerDuties();
+                });
+        })
+    });
+}
+
+function addProduct() {
+    inquirer.prompt([
+        {
+            name: "newProduct",
+            type: "input",
+            message: "What product would you like to add?"
+        },
+        {
+            name: "department",
+            type: "input",
+            message: "In which department should we classify this item?",
+        },
+        {
+            name: "price",
+            type: "input",
+            message: "What is the price per unit? Please enter price in this format: $000.00",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "stockCount",
+            type: "input",
+            message: "How many of this new product are you adding to stock?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function (stockAnswer) {
+        connection.query("INSERT INTO products SET ?",
+            {
+                product_name: stockAnswer.newProduct,
+                department_name: stockAnswer.department,
+                price: stockAnswer.price,
+                stock_quantity: stockAnswer.stockCount
+            },
+            function (err) {
+                if (err) throw (err)
+                console.log(`\n Your new product has been added succesfully! Your inventory now contains:\n`);
+                productsForSale();
+            });
+    })
+}
+
+
+
+
+
